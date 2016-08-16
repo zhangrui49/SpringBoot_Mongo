744d1834acd369b1bb8d18fbae7273d6b1188628
429835306 <429835306@qq.com>
2016-08-16 16:33:43 +0800

    change id type

--- src/main/java/zhangrui/dao/UserDao.java
+++ src/main/java/zhangrui/dao/UserDao.java
@@ -9,6 +9,6 @@ import zhangrui.model.User;
  * Created by zhangrui on 2016/8/16.
  */
 @Transactional
-public interface UserDao extends MongoRepository<User,Long> {
+public interface UserDao extends MongoRepository<User,String> {
     User findByName(String name);
 }
